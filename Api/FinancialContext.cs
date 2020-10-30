7b2da599bbf7708d8aa21c5f7d4bc921bf25d056
Ana Eliza Bastos Martins de Freitas <ana.eliza@squadra.com.br>
2020-10-30 15:36:59 -0300

    Criação usuario controller

--- Api/FinancialContext.cs
+++ Api/FinancialContext.cs
@@ -14,32 +14,27 @@ namespace FinancialWeb.Api
         public FinancialContext(DbContextOptions<FinancialContext> options)
             : base(options)
         {
-        }        
+        }
+
+        public virtual DbSet<Usuario> Usuario { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-               
                 optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Financial");
             }
         }
-
-        public virtual DbSet<Usuario> Usuarios { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Usuario>(entity =>
             {
-                entity.HasNoKey();
-
                 entity.Property(e => e.Cpf)
                     .IsRequired()
                     .HasColumnName("CPF")
                     .HasMaxLength(14);
-
-                entity.Property(e => e.Id).ValueGeneratedOnAdd();
 
                 entity.Property(e => e.NomeCompleto)
                     .IsRequired()
