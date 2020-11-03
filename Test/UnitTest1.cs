a2bdddbf440f2d4db1f1dd949a60a3afdf912bb6
Ana Eliza Bastos Martins de Freitas <ana.eliza@squadra.com.br>
2020-11-03 11:47:38 -0300

    Criando projeto de Testes

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace FinancialWeb.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Padrão AAA:
+
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
